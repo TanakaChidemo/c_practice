812b69754350674efa557b80c6c143fbb031c59b
Tanaka Chidemo <chidemo@icloud.com>
2023-10-10 21:35:16 +0400

    switch statement

---
+++ switch/switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int check_dis;
+    check_dis = 1;
+    switch (check_dis)
+    {
+    case 1:
+        printf("Nas is the 1");
+        break;
+    case 2:
+        printf("Dis be 2 boy!");
+
+    default:
+        printf("Dafukouttahere!");
+        break;
+    }
+}
